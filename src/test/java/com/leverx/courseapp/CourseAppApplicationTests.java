019211d0ec4d771f1013c79c8953e1d53b90e38c
margarita-sk <mskokova2802@gmail.com>
2021-02-01 09:19:13 +0300

    new project

---
+++ src/test/java/com/leverx/courseapp/CourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leverx.courseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
