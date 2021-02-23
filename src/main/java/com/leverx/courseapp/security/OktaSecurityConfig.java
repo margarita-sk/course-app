1ada634b3369cc1384bb366aa1fe87468c04bcda
margarita-sk <mskokova2802@gmail.com>
2021-02-23 13:00:51 +0300

    added okta security

--- src/main/java/com/leverx/courseapp/security/OktaSecurityConfig.java
+++ src/main/java/com/leverx/courseapp/security/OktaSecurityConfig.java
@@ -8,43 +8,54 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OktaSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Value("${okta.client.org-url}")
-  private String oktaDomain;
+    @Value("${okta.client.org-url}")
+    private String oktaDomain;
 
-  @Bean
-  AuthenticationClient authenticationClient() {
-    var client = AuthenticationClients.builder().setOrgUrl(oktaDomain).build();
-    return client;
-  }
+    @Bean
+    AuthenticationClient authenticationClient() {
+        var client = AuthenticationClients.builder().setOrgUrl(oktaDomain).build();
+        return client;
+    }
 
-  //    @Component("userSecurity")
-  //    public class UserSecurity {
-  //        public boolean hasUserEmail(JwtAuthenticationToken authentication, String email) {
-  //            return authentication.getName().equals(email);
-  //        }
-  //    }
+    //    @Component("userSecurity")
+    //    public class UserSecurity {
+    //        public boolean hasUserEmail(JwtAuthenticationToken authentication, String email) {
+    //            return authentication.getName().equals(email);
+    //        }
+    //    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/courses/**", "/tags/**", "/tasks/**")
-        .permitAll()
-        .antMatchers(HttpMethod.POST, "/students")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .logout()
-        .logoutSuccessUrl("/")
-        .and()
-        .oauth2Client()
-        .and()
-        .oauth2Login();
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/courses/**", "/tags/**", "/tasks/**", "/h2/**")
+                .permitAll()
+                .antMatchers(HttpMethod.POST, "/students")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Client()
+                .and()
+                .oauth2Login();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
 }
