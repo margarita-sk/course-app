d7fb4fce314e9e4cd867a31e63c1fa98f7f7f02b
margarita-sk <mskokova2802@gmail.com>
2021-03-01 14:39:28 +0300

    changes in okta security

--- src/main/java/com/leverx/courseapp/user/repository/StudentRepository.java
+++ src/main/java/com/leverx/courseapp/user/repository/StudentRepository.java
@@ -4,4 +4,4 @@ import com.leverx.courseapp.user.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRepository extends JpaRepository<Student, String> {}
+public interface StudentRepository extends CrudRepository<Student, String> {}
